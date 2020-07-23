6576064c91f8eb2cdb49c3e750900dd5762c132e
gregnuj <gregnuj@gmail.com>
2020-07-22 22:57:02 -0500

    added ability to import from file

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,43 @@
-import React from 'react';
-import { Map, Controls, Popup, LayerPanel } from '@bayer/ol-kit';
-import { getWfsLayer } from './geoserver'
+import React, { Component } from 'react'
+import { 
+  Map, 
+  Popup, 
+  Controls, 
+  LayerPanel, 
+  loadDataLayer 
+} from '@bayer/ol-kit'
 
-function App (){
-  const onMapInit = async (map) => {
-    var wfsUrl = process.env.REACT_APP_WFS_URL
-    if (wfsUrl && wfsUrl.length > 0){
-      var wfsLayer = await getWfsLayer(wfsUrl);
-      map.addLayer(wfsLayer);
-    }
+class App extends Component {
+
+  addLayerOnLoaded = async (ev) => {
+    const map = this.state.map
+    const fileName =  ev.target.fileName
+    let content = ev.target.result
+    let layer = await loadDataLayer(map, content, { addToMap: false })
+    layer.set('title', fileName)
+    map.addLayer(layer)
+  }
+  
+  addLayerFromFile = (file) => {
+    let fileData = new FileReader();
+    fileData.fileName = file.name
+    fileData.onloadend = this.addLayerOnLoaded;
+    fileData.readAsText(file);
   }
 
-  return (
-    <Map onMapInit={onMapInit} >
-      <Controls />
-      <Popup />
-      <LayerPanel />  
-    </Map>
-  )
+  onMapInit = (map) => {
+    this.setState({ map: map });
+  }
+
+  render(){
+    return (
+      <Map onMapInit={this.onMapInit} >
+        <Controls />
+        <Popup />
+        <LayerPanel onFileImport={this.addLayerFromFile} />
+      </Map>
+    )
+  }
 }
 
 export default App
